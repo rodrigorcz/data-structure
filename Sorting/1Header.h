8bd9a9a2fe5ebf01702d9d0985e4f49718b725f1
RodrigoRCZ <rodrigo.rcastro27@gmail.com>
2023-05-07 10:55:04 -0300

    adding insertion-sort

--- Sorting/1Header.h
+++ Sorting/1Header.h
@@ -8,3 +8,9 @@ typedef struct {
 void counting_sort(list l);
 void bubble_sort(list l);
 void selection_sort(list l);
+void insertion_sort(list l);
+void merge_sort(list l);
+void quick_sort(list l);
+void shell_sort(list l);
+void radix_sort(list l);
+void heap_sort(list l);
---
+++ Sorting/InsertionSort.c
@@ -0,0 +1,22 @@
+/**
+ *  Time Complexity: O(n) Best case: List already sorted
+ *                   O(n^2) Worst and Average cases
+ *  Space Complexity: O(1)     
+ *  Stability: Yes
+ */
+
+#include "1Header.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void insertion_sort(list l){
+    long i, j, aux;
+
+    for(i = 1; i < l.size; i++){
+        aux = l.data[i]; 
+        for(j = i - 1; j >= 0 && aux < l.data[j]; j--)
+            l.data[j+1] = l.data[j];
+
+        l.data[++j] = aux;
+    }
+}
