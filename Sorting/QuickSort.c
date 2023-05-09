f72f8ffd3817ff663fdd247dc1a9a11ffe4525b1
RodrigoRCZ <rodrigo.rcastro27@gmail.com>
2023-05-09 12:55:42 -0300

    adding quick sort

---
+++ Sorting/QuickSort.c
@@ -0,0 +1,39 @@
+/**
+ *  Time Complexity: O(nlogn)  Best and Average cases
+ *                   O(n^2)    Worts case: smallest or largest indexed element as pivot
+ *  Space Complexity: O(logn)     
+ *  Stability: No
+ */
+
+#include "1Header.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void quick_sort(list l){
+    recursive_quick(l, 0, l.size-1);
+}
+
+void recursive_quick(list l, long ini, long end){
+    long j, i, aux;
+
+    if(ini >= end)
+        return;
+
+    i = ini;
+
+    for(j = ini; j < end; j++)
+        if(l.data[j] < l.data[end]){
+            //swap l.data[i] with l.data[end]
+            aux = l.data[i];
+            l.data[i] = l.data[j];
+            l.data[j] = aux;
+            i++;
+        }
+
+    aux = l.data[i];
+    l.data[i] = l.data[end];
+    l.data[end] = aux;
+
+    recursive_quick(l, ini, i-1);
+    recursive_quick(l, i+1, end);
+}
---
+++ Sorting/ShellSort.c
@@ -0,0 +1,18 @@
+/**
+ *  Time Complexity: O(nlogn) Best and Average cases => n^1.25
+ *                   O(n^2) Worst cases => n(logn)^2
+ *  Space Complexity: O(1)     
+ *  Stability: No
+ */
+
+#include "1Header.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void shell_sort(list l){
+    long i, j;
+    long v, k
+
+
+}
