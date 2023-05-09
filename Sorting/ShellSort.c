ba07895174ad263f2768375f63e66735d9fb7682
RodrigoRCZ <rodrigo.rcastro27@gmail.com>
2023-05-09 15:07:17 -0300

    adding shell sort

--- Sorting/ShellSort.c
+++ Sorting/ShellSort.c
@@ -11,8 +11,31 @@
 
 
 void shell_sort(list l){
-    long i, j;
-    long v, k
+    //index
+    long i, j;   
+    long h, m, k = 0; 
 
+    long elem, *vet = malloc(sizeof(long));
+    vet[k]= 1;
 
+    //intervals vector
+    while(vet[k] < l.size){
+        k++;
+        vet = realloc(vet, sizeof(long) * (k+1));
+        vet = 3 * vet[k-1] + 1; //Knuth
+    }
+    k--;
+
+    for(; k>=0; k--){
+        h = vet[k];
+        for(m = 0; m < h; m++){
+            for(i = h + m; i < l.size; i+=h){
+                elem = l.data[i];
+                for(j = i-h; j>=0 && l.data[j]> elem; j -=h )
+                    l.data[j+h] = l.data[j];
+                
+                l.data[j+h] = elem;
+    }}}
+    
+    free(vet);
 }
