c7aa8ebe9e79d449e9ffefb55d62abeced8849db
RodrigoRCZ <rodrigo.rcastro27@gmail.com>
2023-05-21 17:36:21 -0300

    more search files

--- Searching/LinearSearch.c
+++ Searching/LinearSearch.c
@@ -11,20 +11,54 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-//return -1 = value is not present in the array
+/* 
+    Sequential Search Methods
+
+    Return the position of the element if it is found or return -1
+*/
 
 int sequential_search(int *vet, int size, int elem){
     int pos;
     for(pos = 0; pos<size; pos++)
         if(vet[pos] == elem)
-            break;
-    return (pos < size) ? pos : -1; 
+            return pos;
+    return -1; 
 }
 
 int sentinel_sequential_search(int *vet, int size, int elem){
     int pos;
-    vet[size] = elem; //sentinel 
+    vet[size] = elem; //sentinel in last position
     for(pos = 0; vet[pos]!=elem; pos++)
     return (pos < size) ? pos : -1; 
+}
+
+/* 
+    Self Organization Methods 
+
+    Improve efficiency of linear search moving frequently items toward the head of the list
+    
+*/
+
+bool move_to_front_search(int *vet, int size, int elem){
+    int aux, i;
+    for(i = 0; i< size; i++)
+        if(vet[i] == elem){
+            
+        }
+    
+}
+
+bool transpose_search(int *vet, int size, int elem){
+    int aux, i;
+
+    for(i = 0; i<size; i++)
+        if(vet[i] == elem){
+            aux = vet[i-1];
+            vet[i-1] = vet[i];
+            vet[i] = aux;
+            return true;
+        }
+    return false;
 }
